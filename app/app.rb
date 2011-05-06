99d45e120e64af32ad2103e25acfc39949fb748d
udzura <udzura@udzura.jp>
2011-05-06 13:14:03 +0900

    add japanese admin text

--- app/app.rb
+++ app/app.rb
@@ -13,7 +13,7 @@ class PadrinoWeb < Padrino::Application
     :authentication  => :login # :plain, :login, :cram_md5, no auth by default
   }
   set :exceptions_subject, "PadrinoWeb"
-  set :exceptions_from,    "Padrino WebSite <exceptions@padrinorb.com>"
+  set :exceptions_from,    "Padrino WebSite <exceptions@jp.padrinorb.com>"
   set :exceptions_to,      Account.all.map(&:email)
   set :exceptions_page,    "base/errors"
   # # Uncomment this for test in development
